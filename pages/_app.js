b193112a036093e289c7d3f3cf0a3947db0a7933
SanityZeroPercent <nmatthewsoft@gmail.com>
2023-04-27 15:19:07 +0700

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,13 @@ import "../assets/css/scrolling-nav.css";
 import "../assets/css/animate.min.css";
 import "../assets/css/radial.css";
 import "../assets/css/style.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
