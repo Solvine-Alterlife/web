7cb483f33e515b330162427916aba4adfa9935c8
SanityZeroPercent <nmatthewsoft@gmail.com>
2023-04-27 14:09:09 +0700

    sync asset again

--- assets/components/Heading.jsx
+++ assets/components/Heading.jsx
@@ -5,7 +5,7 @@ import Script from 'next/script'
 export const Heading = () => {
   return (
     <>
-        <Head>
+      <Head>
         <meta charSet="utf-8" />
         <title>Solvine Alterlife - Discord Leaderboard</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -15,14 +15,14 @@ export const Heading = () => {
         <meta name="robots" content="index, follow" />
         <meta name="revisit-after" content="3 days" />
         <meta name="author" content="clemiee" />
-        <meta property="og:image" content="../wi-logo.png" />
-        </Head>
-        <Script src="../assets/js/discord.js"></Script>
-        <Script src="../assets/js/jquery.easing.min.js"></Script>
-        <Script src="../assets/js/script.js"></Script>
-        <Script src="../assets/js/scrolling-nav.js"></Script>
-        <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
-        <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></Script>
+        <meta property="og:image" content="../solvine-logo.png" />
+      </Head>
+      <Script src="../assets/js/discord.js"></Script>
+      <Script src="../assets/js/jquery.easing.min.js"></Script>
+      <Script src="../assets/js/script.js"></Script>
+      <Script src="../assets/js/scrolling-nav.js"></Script>
+      <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></Script>
     </>
   );
 };
